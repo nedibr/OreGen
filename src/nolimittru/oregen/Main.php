2e56cae20268d00124e4351a2c62c55223ae6006
= <nedalibrahim982@gmail.com>
2020-12-29 21:41:53 -0500

    yes

--- src/nolimittru/oregen/Main.php
+++ src/nolimittru/oregen/Main.php
@@ -20,7 +20,7 @@ class Main extends PluginBase
 
     public function registerClasses(): void{
         $this->config = new Config($this->getDataFolder() . "config.json", Config::JSON);
-        $this->oreManager = new OreManager($this, $this->config->get("arenas"));
+        $this->oreManager = new OreManager($this, $this->config->get("worlds"));
         $this->getServer()->getPluginManager()->registerEvents(new GenListener($this), $this);
     }
 
